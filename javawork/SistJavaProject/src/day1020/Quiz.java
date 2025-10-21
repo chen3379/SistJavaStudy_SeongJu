4f495af62f9e361f9debff76550dad6dafc11947
seongju <premium71712@gmail.com>
2025-10-21 09:01:25 +0900

    Quiz까지의 파일 추가

--- javawork/SistJavaProject/src/day1020/Quiz.java
+++ javawork/SistJavaProject/src/day1020/Quiz.java
@@ -14,12 +14,29 @@ public class Quiz {
 		 가격: 1500000원
 		 총금액: 3000000원*/
 		
-		//1.변수선언
+		// 1.변수선언
 		
-		//2.계산
+		String name = args[0];
+		int num = Integer.parseInt(args[1]);
+		int price = Integer.parseInt(args[2]);
 		
-		//3.출력
+		// 2.계산
+		
+		int total_price = num*price;
+		
+		// 3.출력
+		
+		// 1)
+		System.out.printf("상품명: %s\n수량: %d개\n가격: %d원\n총금액: %d원\n", name, num, price, total_price);
 
+		// 2)
+		System.out.printf("상품명: "+args[0]+"\n수량: "+args[1]+"개\n가격: "+args[2]+"원\n총금액: "+total_price+"원");
+		
+		// 3)
+		System.out.println("\n상품명: "+args[0]);
+		System.out.println("수량: "+args[1]+"개");
+		System.out.println("가격: "+args[2]+"원");
+		System.out.println("총금액: "+total_price+"원");
 	}
 
 }
